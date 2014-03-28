55f8fa1c0c62185a6ebf14e6ab4eec1193f5d0f0
yangjeewoong <danielay@nhnnext.org>
2014-03-29 03:36:51 +0900

    Digital clock coded and simple refactoring

--- clock.js
+++ clock.js
@@ -1,30 +1,36 @@
-function DrawWord(x,y,word){
+function DrawWord(x,y){
   this.x = x;
   this.y = y;
-  this.word = word;
   this.canvas = document.getElementById('myCanvas');
   this.context = this.canvas.getContext('2d');
 }
 
 DrawWord.prototype.refresh = function() {
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
-  this.word = new Date().getSeconds();
+  this.now = new Date();
 }
 
-DrawWord.prototype.draw = function() {
+DrawWord.prototype.print = function() {
   this.refresh();
   this.context.fillStyle = 'black';
   this.context.font = "35px arial";
   this.context.textBaseline = "top";
-  this.context.fillText(this.word, this.x, this.y);
+  var t = this.now;
+  this.pos_x = this.x;
+  //this & var ?
+  var timeline = [t.getHours(), ':', t.getMinutes(), ':', t.getSeconds()];
+  
+  for(var i in timeline)
+  {
+    this.context.fillText(timeline[i],this.pos_x,this.y);
+    this.pos_x += charspace/2;
+    if (i % 2 == 0) this.pos_x += charspace/2;
+  }
 }
 
-var time = new Date();
-var obj1 = new DrawWord(10,10,time.getHours());
-var obj2 = new DrawWord(50,10,time.getMinutes());
-var obj3 = new DrawWord(90,10,time.getSeconds());
+var clock = new DrawWord(10,10);
+var charcolor = 'black';
+var font = '35px arial';
+var charspace = 50; //px
 
-obj1.draw();
-obj2.draw();
-setInterval("obj3.draw()", 1000);
-
+setInterval("clock.print()", 1000);
