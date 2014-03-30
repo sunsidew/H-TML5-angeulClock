8447835d7500834ede00acb09852678f9836f29a
yangjeewoong <danielay@nhnnext.org>
2014-03-31 05:08:18 +0900

    midnight, noon function add

--- Hangeul_clock.js
+++ Hangeul_clock.js
@@ -15,17 +15,20 @@ function DrawBoard(x,y) {
 DrawBoard.prototype.TimeParse = function() {
   this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
   var now = new Date();
+  
   var h = (now.getHours()-1)%12+1;
   var m = now.getMinutes();
   
-  console.log(this.mm);
+  var mid = h%12===0;
+  var oclock = (m>=0&&m<5);
   
+  console.log(h,m);
   this.circuit = [
-    [h>=10,h%10===1,h===5,h===3,h===4],
-    [h%10===2,h===6,h===5||h===6,h===7,h===7],
-    [h==8,h===8,h===9,h===9,h!==0],
-    [h===0,h===0,m>=20&&m<30,m>=30&&m<40,(m>=10&&m<20)||(m>=30&&m<40)],
-    [m>=40&&m<50,m>=50,(m>=20&&m<30)||m>=40,m%10>=5,m>=5]
+    [h>=10||(mid&&!oclock),h%10===1,h===5,h===3,h===4],
+    [h%10===2||(mid&&!oclock),h===6,h===5||h===6,h===7,h===7],
+    [h===8,h===8,h===9,h===9,!mid||!oclock],
+    [h===0&&oclock,mid&&oclock,m>=20&&m<30,m>=30&&m<40,(m>=10&&m<20)||(m>=30&&m<40)],
+    [m>=40&&m<50,h==12&&oclock||m>=50,(m>=20&&m<30)||m>=40,m%10>=5,m>=5]
   ];
   
   this.FrameDraw();
